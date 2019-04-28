de483a9bc825ddd661cab601226ced8c3c643619
buecktobias <44665917+buecktobias@users.noreply.github.com>
2019-04-28 14:23:18 +0200

    improved visualization

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def make_semi_circle_list(triangles):
 
 
 def set_triangle_position_on_the_road(triangle: Triangle, point_at_which_semi_circle_is_placed, sum_of_angles_before):
-    point_with_sharpest_angle =  (point_at_which_semi_circle_is_placed, STREET_Y_POSITION)
+    point_with_sharpest_angle = (point_at_which_semi_circle_is_placed, STREET_Y_POSITION)
     point_at_end_of_hypotenuse = (point_with_sharpest_angle[0] - float(cos(radians(sum_of_angles_before)) * triangle.hypotenuse_length), point_with_sharpest_angle[1] + float((sin(radians(sum_of_angles_before)) * triangle.hypotenuse_length)))
     point__between_the_cathets = (point_with_sharpest_angle[0] - float((cos(radians(triangle.sharpest_angle + sum_of_angles_before)) * triangle.sharpest_angle_adjacent_length)), point_with_sharpest_angle[1] + float((sin(radians(triangle.sharpest_angle + sum_of_angles_before)) * triangle.sharpest_angle_adjacent_length)))
     triangle.a_point = Point(point_with_sharpest_angle)
@@ -59,7 +59,7 @@ def is_triangle_intersecting_triangles_from_former_semi_circle(triangle, former_
 
 
 def set_triangles_on_the_road(semi_circle_list, canvas):
-    point_at_which_semi_circle_is_placed = 0
+    point_at_which_semi_circle_is_placed = 100
     for i, semi_circle in enumerate(semi_circle_list):
         if i == 0:
             semi_circle.triangles.sort(key=lambda triangle: triangle.hypotenuse_length, reverse=True)
@@ -87,34 +87,44 @@ def set_triangles_on_the_road(semi_circle_list, canvas):
         point_at_which_semi_circle_is_placed += semi_circle.triangles[-1].sharpest_angle_adjacent_length
 
 
-def measure_distance_and_make_line(semi_circle_list,canvas):
+def measure_distance_and_make_line(semi_circle_list, canvas):
     first_property = semi_circle_list[0].triangles[0]
     first_property_door = first_property.a_point
 
     last_property = semi_circle_list[-1].triangles[-1]
     last_property_door = last_property.a_point
-    canvas.create_line([round(first_property_door.x), STREET_Y_POSITION - 100], [round(last_property_door.x), STREET_Y_POSITION - 100])
-    return abs(first_property_door.x - last_property_door.y)
+    difference = abs(first_property_door.x - last_property_door.x)
+    canvas.create_line([round(first_property_door.x), STREET_Y_POSITION - 30], [round(last_property_door.x), STREET_Y_POSITION - 30])
+    canvas.create_text([round(last_property_door.x - difference / 2), STREET_Y_POSITION - 40], text=round(difference))
+    return difference
+
+
+def draw_street(canvas, canvas_width):
+    canvas.create_line([0, STREET_Y_POSITION - 1], [canvas_width, STREET_Y_POSITION - 1])
+    canvas.pack()
+    canvas.update()
+
 
 def main():
-    triangles = get_triangles_from_file("dreiecke4.txt")
+    triangles = get_triangles_from_file("dreiecke5.txt")
     triangles = [Triangle(*triangle, "D" + str(counter+1)) for counter, triangle in enumerate(triangles)]
     root: tk.Tk = tk.Tk()
-    canvas_width: int = 1000
+    canvas_width: int = 1500
     canvas_height: int = 800
     canvas: tk.Canvas = tk.Canvas(root, width=canvas_width, height=canvas_height)
+    draw_street(canvas, canvas_width)
     semi_circle_list = make_semi_circle_list(triangles)
-    colors = ["yellow", "red", "blue", "green", "black"]
+    colors = ["yellow", "red", "blue", "green", "black", "orange", "grey"]
     for counter, semi_circle in enumerate(semi_circle_list):
         semi_circle.show(canvas, colors[counter])
     set_triangles_on_the_road(semi_circle_list, canvas)
-    distance_between_first_huse_and_last_house = float(measure_distance_and_make_line(semi_circle_list, canvas))
+    distance_between_first_house_and_last_house = float(measure_distance_and_make_line(semi_circle_list, canvas))
     root.mainloop()
     str_triangles_best_positions = [[f"{triangle.name}: ({round(float(triangle.a_point.x))}|{round(float(triangle.a_point.y))}), ({round(float(triangle.b_point.x))}|{round(float(triangle.b_point.y))}), ({round(float(triangle.c_point.x))}|{round(float(triangle.c_point.y))})" for triangle in semi_circle_.triangles] for semi_circle_ in semi_circle_list]
-    print(str_triangles_best_positions)
+    print(f"{round(distance_between_first_house_and_last_house)}, {str_triangles_best_positions}")
 
 
 if __name__ == '__main__':
-    STREET_Y_POSITION = 400
+    STREET_Y_POSITION = 200
     MAXIMUM_ANGLE_SEMI_CIRCLE = 180
     main()
